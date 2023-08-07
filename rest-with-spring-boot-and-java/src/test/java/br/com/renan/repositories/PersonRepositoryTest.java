5c2540f7ee5ec3f07d289d2039119d1ba23b9f2e
Renan <orenan124@gmail.com>
2023-08-07 11:58:00 -0300

    ajustado o problema dos testes e finalizando teste da camada repository

--- rest-with-spring-boot-and-java/src/test/java/br/com/renan/repositories/PersonRepositoryTest.java
+++ rest-with-spring-boot-and-java/src/test/java/br/com/renan/repositories/PersonRepositoryTest.java
@@ -1,18 +1,16 @@
 package br.com.renan.repositories;
 
 import br.com.renan.models.Person;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class PersonRepositoryTest {
@@ -20,43 +18,194 @@ class PersonRepositoryTest {
     @Autowired
     private PersonRepository personRepository;
 
-    private Person person0;
 
-//    @DisplayName("Given person object when save then return saved person")
-//    @Test
-//    void testGivenPersonObject_whenSave_thenReturnSavedPerson(){
-//        //Given / Arrange
-//        Person person = new Person("Andre",
-//                "Costa",
-//                "Osasco - São Paulo - Brasil",
-//                "Male",
-//                "andre@email.com");
-//
-//        //When / Act
-//        Person savedPerson = personRepository.save(person);
-//        //Then / Assert
-//        assertNotNull(savedPerson);
-//        assertTrue(savedPerson.getId() > 0);
-//
-//    }
+    private Person person;
+
+    @BeforeEach
+    public void setup(){
+        person = new Person(
+            "Leandro",
+            "Costa",
+            "leandro@erudio.com.br",
+            "Uberlândia - Minas Gerais - Brasil",
+            "Male");
+    }
 
     @DisplayName("JUnit test for Given Person Object when Save then Return Saved Person")
     @Test
     void testGivenPersonObject_whenSave_thenReturnSavedPerson() {
 
         // Given / Arrange
-        person0 = new Person(
+
+        // When / Act
+        Person savedPerson = personRepository.save(person);
+
+        // Then / Assert
+        assertNotNull(savedPerson);
+        assertTrue(savedPerson.getId() > 0);
+    }
+
+    @DisplayName("JUnit test for Given Person List when findAll then Return Person List")
+    @Test
+    void testGivenPersonList_whenFindAll_thenReturnPersonList() {
+
+        // Given / Arrange
+        Person person1 = new Person(
                 "Leandro",
                 "Costa",
                 "leandro@erudio.com.br",
                 "Uberlândia - Minas Gerais - Brasil",
                 "Male");
 
+        personRepository.save(person);
+        personRepository.save(person1);
+
         // When / Act
-        Person savedPerson = personRepository.save(person0);
+        List<Person> personList = personRepository.findAll();
+
+        // Then / Assert
+        assertNotNull(personList);
+        assertEquals(2, personList.size());
+    }
+
+    @DisplayName("JUnit test for Given Person Object when findById then Return Person Object")
+    @Test
+    void testGivenPersonObject_whenFindById_thenReturnPersonObject() {
+
+        // Given / Arrange
+
+        personRepository.save(person);
+        // When / Act
+        Person savedPerson = personRepository.findById(person.getId()).get();
 
         // Then / Assert
         assertNotNull(savedPerson);
-        assertTrue(savedPerson.getId() > 0);
+        assertEquals(savedPerson.getId(), person.getId());
+    }
+
+    @DisplayName("JUnit test for Given Person Object when findByEmail then Return Person Object")
+    @Test
+    void testGivenPersonObject_whenFindByEmail_thenReturnPersonObject() {
+
+        // Given / Arrange
+
+        personRepository.save(person);
+        // When / Act
+        Person savedPerson = personRepository.findByEmail(person.getEmail()).get();
+
+        // Then / Assert
+        assertNotNull(savedPerson);
+        assertEquals(savedPerson.getEmail(), person.getEmail());
+    }
+
+    @DisplayName("JUnit test for Given Person Object when updatePerson then Return Updated Person Object")
+    @Test
+    void testGivenPersonObject_whenUpdatePerson_thenReturnUpdatedPersonObject() {
+
+        // Given / Arrange
+
+        personRepository.save(person);
+        // When / Act
+        Person savedPerson = personRepository.findById(person.getId()).get();
+        savedPerson.setFirstName("Leonardo");
+        savedPerson.setEmail("leonardo@erudio.com.br");
+
+        Person updatedPerson = personRepository.save(savedPerson);
+
+        // Then / Assert
+        assertNotNull(savedPerson);
+        assertEquals("Leonardo", updatedPerson.getFirstName());
+        assertEquals("leonardo@erudio.com.br", updatedPerson.getEmail());
+    }
+
+    @DisplayName("JUnit test for Given Person Object when delete then remove Person")
+    @Test
+    void testGivenPersonObject_whenDelete_thenRemovePerson() {
+
+        // Given / Arrange
+
+        personRepository.save(person);
+        // When / Act
+        personRepository.deleteById(person.getId());
+
+        Optional<Person> personOptional = personRepository.findById(person.getId());
+        // Then / Assert
+        assertTrue(personOptional.isEmpty());
+    }
+
+    @DisplayName("JUnit test for Given Firstname And LastName when FindByJPQL then Return Person Object")
+    @Test
+    void testGivenFirstnameAndLastName_whenFindByJPQL_thenReturnPersonObject() {
+
+        // Given / Arrange
+
+        String firstName = "Leandro";
+        String lastName = "Costa";
+
+        personRepository.save(person);
+        // When / Act
+        Person savedPerson = personRepository.findByJPQL(firstName, lastName);
+
+        // Then / Assert
+        assertNotNull(savedPerson);
+        assertEquals(firstName, person.getFirstName());
+        assertEquals(lastName, person.getLastName());
+    }
+
+    @DisplayName("JUnit test for Given Firstname And LastName when FindByJPQLNamedParameters then Return Person Object")
+    @Test
+    void testGivenFirstnameAndLastName_whenFindByJPQLNamedParameters_thenReturnPersonObject() {
+
+        // Given / Arrange
+
+        String firstName = "Leandro";
+        String lastName = "Costa";
+
+        personRepository.save(person);
+        // When / Act
+        Person savedPerson = personRepository.findByJPQLNamedParameters(firstName, lastName);
+
+        // Then / Assert
+        assertNotNull(savedPerson);
+        assertEquals(firstName, person.getFirstName());
+        assertEquals(lastName, person.getLastName());
+    }
+
+    @DisplayName("JUnit test for Given Firstname And LastName when FindByNativeSQL then Return Person Object")
+    @Test
+    void testGivenFirstnameAndLastName_whenFindByNativeSQL_thenReturnPersonObject() {
+
+        // Given / Arrange
+
+        String firstName = "Leandro";
+        String lastName = "Costa";
+
+        personRepository.save(person);
+        // When / Act
+        Person savedPerson = personRepository.findByNativeSQL(firstName, lastName);
+
+        // Then / Assert
+        assertNotNull(savedPerson);
+        assertEquals(firstName, person.getFirstName());
+        assertEquals(lastName, person.getLastName());
+    }
+
+    @DisplayName("JUnit test for Given Firstname And LastName when FindByNativeSQLWithNamedParameters then Return Person Object")
+    @Test
+    void testGivenFirstnameAndLastName_whenFindByNativeSQLWithNamedParameters_thenReturnPersonObject() {
+
+        // Given / Arrange
+
+        String firstName = "Leandro";
+        String lastName = "Costa";
+
+        personRepository.save(person);
+        // When / Act
+        Person savedPerson = personRepository.findByNativeSQLWithNamedParameters(firstName, lastName);
+
+        // Then / Assert
+        assertNotNull(savedPerson);
+        assertEquals(firstName, person.getFirstName());
+        assertEquals(lastName, person.getLastName());
     }
 }
