8b7c31f5a4784a0ebde3580452ceb7ae41bb43d4
Renan <orenan124@gmail.com>
2023-08-07 15:43:52 -0300

    adicionado projeto simples usado para aprender o basico de docker

---
+++ java-spring-boot-docker/src/test/java/br/com/renan/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.renan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
